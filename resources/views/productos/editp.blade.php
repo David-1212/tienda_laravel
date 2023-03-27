7811ee2464453761045ab059b0319a0f7104497d
ulises vallejo <jose.vallejo7479@alumnos.udg.mx>
2023-03-27 02:35:48 -0600

    vista editar funciona

--- resources/views/productos/editp.blade.php
+++ resources/views/productos/editp.blade.php
@@ -1,44 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar</title>
-</head>
-<body>
-    <h1>Editar productos</h1>
-    <form action="/producto/{{ $producto->id }}" method="POST">
-    @csrf
-    @method('PATCH')
-    
-        <label for="nombrep">Nombre:</label><br>
-        <input type="text" name = "nombrep" value="{{old('nombrep')?:$producto->nombrep }}" id = "nombrep"  ><br><br>
-       
-        @error('nombrep')
-            <h5>{{ $message }}</h5>
-        @enderror
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Editar</title>
+        <link href="{{asset('template/css/styles.css')}}" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Editar producto</h3></div>
+                                    <div class="card-body">
+                                        <form action="/producto/{{ $producto->id }}" method="post">
+                                            @csrf
+                                            @method('PATCH')
+                                            <div class="form-floating mb-3">
+                                                <label for="nombrep">Nombre:</label><br><br>
+                                                <input type="text" name = "nombrep" id = "nombrep" class="form-control" value ="{{old('nombrep') ?? $producto->nombrep }}">
+                                                @error('nombrep')
+                                                    <h5>{{ $message }}</h5>
+                                                @enderror
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <label for="precio">Precio:</label><br><br>
+                                                <input type="double" name = "precio" class="form-control" id = "precio" value ="{{old('precio')  ?? $producto->precio }}">
+                                                @error('precio')
+                                                    <h5>{{ $message }}</h5>
+                                                @enderror
+                                               
+                                                
+                                            </div>
+                                                <div class="form-floating mb-3">
+                                                <label for="descripcion">Descripción:</label><br><br>
+                                                <input type="text" class="form-control" name = "descripcion" id = "descripcion" value ="{{old('descripcion')  ?? $producto->descripcion }}"><br><br>
+                                                @error('descripcion')
+                                                    <h5>{{ $message }}</h5>
+                                                @enderror
+                                            </div>
+                                            
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
 
-        <br>
-
-        <label for="precio">Precio:</label><br>
-        <input type="double" name = "precio" id = "precio" value="{{old('precio')?:$producto->precio }} "><br><br>
-        @error('precio')
-            <h5>{{ $message }}</h5>
-        @enderror
-
-        <br>
-
-        <label for="descripcion">Descripción:</label><br>
-        <input type="text" name = "descripcion" id = "descripcion" value="{{old('descripcionp')?:$producto->descripcion }}"><br><br>
-        @error('descripcion')
-            <h5>{{ $message }}</h5>
-        @enderror
-
-        <br>
-
-        <input type="submit" value="Enviar">
-    </form>
-       
-</body>
+                                            <input type="submit" value="Enviar"class="btn btn-primary">
+                                            </div>
+                                        </form>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2021</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{asset('template/js/scripts.js')}}"></script>
+    </body>
 </html>
